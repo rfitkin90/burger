29053a17b390d792303094f063ace5beda51e6e2
Richard Fitkin <rfitkin90@gmail.com>
2019-02-09 14:09:05 -0800

    added file structure

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+DROP TABLE IF EXISTS burgers;
+CREATE TABLE burgers (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    burger_name VARCHAR(40) NOT NULL,
+    devoured bit NOT NULL
+);
