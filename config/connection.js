2c5bd6af0a487f116fcb5703de4038dfb68ec3e7
Richard Fitkin <rfitkin90@gmail.com>
2019-02-19 15:15:26 -0800

    added reconnect logic

--- config/connection.js
+++ config/connection.js
@@ -23,12 +23,37 @@ if (process.env.NODE_ENV == 'production') {
 console.log(process.env.NODE_ENV);
 console.log(config);
 
-var connection = mysql.createConnection(config);
+// var connection = mysql.createConnection(config);
 
-connection.connect(function (err) {
-   if (err) throw err;
-   console.log(`connected as id: ${connection.threadId}`);
-});
+var connection;
+
+function handleDisconnect() {
+   connection = mysql.createConnection(config); // Recreate the connection, since
+   // the old one cannot be reused.
+
+   connection.connect(function (err) {              // The server is either down
+      if (err) {                                     // or restarting (takes a while sometimes).
+         console.log('error when connecting to db:', err);
+         setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+      }                                     // to avoid a hot loop, and to allow our node script to
+   });                                     // process asynchronous requests in the meantime.
+   // If you're also serving http, display a 503 error.
+   connection.on('error', function (err) {
+      console.log('db error', err);
+      if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+         handleDisconnect();                         // lost due to either server restart, or a
+      } else {                                      // connnection idle timeout (the wait_timeout
+         throw err;                                  // server variable configures this)
+      }
+   });
+}
+
+handleDisconnect();
+
+// connection.connect(function (err) {
+//    if (err) throw err;
+//    console.log(`connected as id: ${connection.threadId}`);
+// });
 
 // export connection for orm.js to use
 module.exports = connection;
